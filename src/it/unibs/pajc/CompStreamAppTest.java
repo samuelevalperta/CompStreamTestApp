0272fc0e06061d05383b26afa99f6cf035e6b38e
Samuele Valperta <samuele.valperta@gmail.com>
2023-10-04 11:31:22 +0200

    Improve efficiency of stream operations
    Refactored the sequence of stream operations in CompStreamAppTest.java. Rather than chaining all operations into one line, the resulting stream is assigned to a variable 'myStream'. This boosts the efficiency and flexibility of the entire stream operation. This modification also enhances readability and maintainability of the code.

--- src/it/unibs/pajc/CompStreamAppTest.java
+++ src/it/unibs/pajc/CompStreamAppTest.java
@@ -1,13 +1,15 @@
 package it.unibs.pajc;
 
 import java.util.*;
+import java.util.stream.Stream;
+
 public class CompStreamAppTest {
 
     public static void main(String[] args) {
         List<String> list = Arrays.asList("a1", "a2", "b1", "c1", "c2");
 
         // This is stateless
-        list
+        Stream<String> myStream = list
                 .stream()
                 .map((a) -> {
                     System.out.printf("\nLOG: MAP: %s", a);
@@ -16,9 +18,10 @@ public class CompStreamAppTest {
                 .filter((a) -> {
                     System.out.printf("\nLOG: FLT: %s", a);
                     return !a.startsWith("B");
-                })
-                .sorted()
-                .forEach((s) -> System.out.printf("\nOUTPUT: %s", s));
+                });
+                //.forEach((s) -> System.out.printf("\nOUTPUT: %s", s));
+
+        myStream.forEach(s -> System.out.printf("\nOUTPUT: %s", s));
 
     }
 }
