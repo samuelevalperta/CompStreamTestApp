78703ffbeb009a5e106b418846934d2417b9531b
Samuele Valperta <samuele.valperta@gmail.com>
2023-10-04 10:50:46 +0200

    Filtered with a new intermediate ArrayList

--- src/it/unibs/pajc/CompStreamAppTest.java
+++ src/it/unibs/pajc/CompStreamAppTest.java
@@ -6,11 +6,17 @@ public class CompStreamAppTest {
     public static void main(String[] args) {
         List<String> list = Arrays.asList("a1", "a2", "b1", "c1", "c2");
 
+        ArrayList<String> list2 = new ArrayList<>();
+
         for (String s: list
              ) {
-            if (!s.startsWith("b")) {
+            if (!s.startsWith("b"))
+                list2.add(s);
+        }
+
+        for (String s: list2
+             ) {
                 System.out.println(s);
             }
         }
-    }
 }
